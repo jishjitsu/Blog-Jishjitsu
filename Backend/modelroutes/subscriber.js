9a638f5733ad0e8e4664d2e2621868619894aee3
jishjitsu <123642123+jishjitsu@users.noreply.github.com>
2024-07-19 16:19:54 +0530

    Delete Backend directory

--- Backend/modelroutes/subscriber.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const subscriberSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-});
-
-module.exports = mongoose.model('Subscriber', subscriberSchema);
